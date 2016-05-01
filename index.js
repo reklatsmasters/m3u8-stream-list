9964f9d66f0ec014bbf4e3fa8e62c656620eeaa2
ReklatsMasters <dmitrycvet@gmail.com>
2016-05-01 21:19:09 +0500

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const TAG_STREAM = "#EXT-X-STREAM-INF:"
-const NON_QUOTED_COMMA = /,(?=(?:[^"]|"[^"]*")*$)/
-const KV_SPLITTER = /="?([^"]*)/
+const reader = require('m3u8-reader')
+
+const TAG_STREAM = "STREAM-INF"
 
 module.exports = m3u8
 
@@ -12,19 +12,7 @@ module.exports = m3u8
  * @return {Array.<{url: string}>}
  */
 function m3u8(playlist) {
-  const lines = playlist.toString().split('\n')
-  const stream_list = []
-
-  for (const stream of split_streams(lines)) {
-    if (is_url(stream.url)) {
-      const params = parse_params(stream.line)
-      params.url = stream.url
-
-      stream_list.push(params)
-    }
-  }
-
-  return stream_list
+  return split_streams(reader(playlist))
 }
 
 /**
@@ -32,19 +20,16 @@ function m3u8(playlist) {
  * @return {Array.<{url:string, line:string}>}
  */
 function split_streams(lines) {
-  if (!Array.isArray(lines)) {
-    throw new TypeError('Expected array of lines')
-  }
+  return lines.reduce(function (streams, url, i, pl) {
+    if (is_url(url)) {
+      const stream = pl[i - 1]
 
-  return lines.reduce(function (streams, line, i, pl) {
-    if (!line.startsWith(TAG_STREAM)) {
-      return streams
+      if ( typeof stream == 'object' && stream[TAG_STREAM] && typeof stream[TAG_STREAM] == 'object' ) {
+        streams.push( Object.assign({url}, stream[TAG_STREAM]) )
+      }
     }
 
-    if (i + 1 < pl.length) {
-      streams.push({ line, url: pl[i + 1] })
-      return streams
-    }
+    return streams
   }, [])
 }
 
@@ -53,25 +38,5 @@ function split_streams(lines) {
  * @return {boolean}
  */
 function is_url(url) {
-  return url.startsWith('http://') || url.startsWith('https://')
-}
-
-/**
- * @param {string} line
- */
-function parse_params(line) {
-  if (!line.startsWith(TAG_STREAM)) {
-    throw new Error('Expected stream line')
-  }
-
-  const pairs = line.slice(18).split(NON_QUOTED_COMMA)
-  const attrs = {}
-
-  for (const kv of pairs) {
-    const kv_list = kv.split(KV_SPLITTER)
-
-    attrs[ kv_list[0].trim() ] = kv_list[1].trim()
-  }
-
-  return attrs
+  return (typeof url == 'string') && (url.startsWith('http://') || url.startsWith('https://'))
 }
